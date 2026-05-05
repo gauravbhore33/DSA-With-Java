380273164ab1e9d53374b8c5b26e5eff520fd042
Gaurav Bhore <gauravbhore95@gmail.com>
2026-05-05 23:25:01 +0530

    Day 5: Improved input handling and added new logic

--- Lecture-05-InputOutput/Takinginput.java
+++ Lecture-05-InputOutput/Takinginput.java
@@ -1,9 +1,32 @@
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
 public class Takinginput {
     static void main() {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter FirstNum:");
+        int FirstNum=sc.nextInt();
+        System.out.println("Enter SecondNum:");
+        int SecondNum=sc.nextInt();
         
-    
-    int a = 5;
-    int b = 5;
-    System.out.println(a+b);
-}
+        int ans=FirstNum+SecondNum;
+        System.out.println("Answer is:"+ans);
+
+        // Various Values like integers:
+
+        System.out.println("Enter Value for Big Interger:");
+        BigInteger bg=sc.nextBigInteger();
+        System.out.println("Enter value for Flag:");
+        Boolean Flag=sc.nextBoolean();
+        System.out.println("Enter a Short Value:");
+        Short shortval=sc.nextShort();
+        System.out.println("Enter a Float Value:");
+        Float floatvalue=sc.nextFloat();
+
+        System.out.println("Big Integer is:"+bg);
+        System.out.println("Flag is:"+Flag);
+        System.out.println("Short Value is:"+shortval);
+        System.out.println("Float Value is:"+floatvalue);
+    }
 }
